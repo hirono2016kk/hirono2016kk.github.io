9430182290341d69f798bdd5f8a57500445d534f
hideki0001 <hideki0001@gmail.com>
2018-03-27 17:14:23 +0900

    update 2018-03-27_1714 a66-Diginnos-PC

---
+++ _plugins/helloworld.rb
@@ -0,0 +1,22 @@
+
+module Jekyll
+	class HelloWorldPage < Page
+		def initialize(site, base, dir)
+			@site, @base, @dir = site, base, dir
+      t = Time.now.strftime("%Y-%m-%d-%H_%M")
+			@name = "helloworld_#{t}.html"
+
+			self.process(@name)
+			self.content = '<html><body>Hello, World!</body></html>'
+			self.data = {}
+		end
+	end
+
+	class HelloWorldGenerator < Generator
+		safe true
+
+		def generate(site)
+			site.pages << HelloWorldPage.new(site, site.source, '/')
+		end
+	end
+end
